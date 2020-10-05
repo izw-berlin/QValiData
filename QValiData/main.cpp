d6dd9768a5c5843b346ace5ef0216f5c5593648f
jchen <jchen2120212@gmail.com>
2020-10-05 00:00:03 -0400

    Set organization name

--- QValiData/main.cpp
+++ QValiData/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setOrganizationName("jchen-cs");
+    a.setOrganizationDomain("jchen-cs");
     MainWindow w;
     w.show();
 
